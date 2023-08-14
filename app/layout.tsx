a1f6fbf10b0ecc0c537e2547166fe27448d4d586
Roy Zhang <roysmiley.ca@gmail.com>
2023-08-13 20:30:04 -0400

    gg

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,14 +42,14 @@ export default function RootLayout({
     <html lang="en">
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
       />
 
       <Script id='google-analytics' strategy="lazyOnload"> {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}', {
           page_path: window.location.pathname,
           });
       `}
