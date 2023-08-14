0f2fae40a48e548bf965cb16edb2c8c5afe83784
Roy Zhang <roysmiley.ca@gmail.com>
2023-08-13 20:27:32 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import Head from 'next/head';
 // import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -38,6 +40,21 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id='google-analytics' strategy="lazyOnload"> {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+      `}
+      </Script>
+
       <body className={inter.className}>{children}</body>
     </html>
   )
