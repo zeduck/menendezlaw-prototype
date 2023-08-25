30b784b296552573681f83a5f45839a7f03f00a4
Roy Zhang <roysmiley.ca@gmail.com>
2023-08-25 18:08:12 -0400

    organize

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,23 +1,19 @@
 'use client';
 import Script from "next/script";
 
-export default function GoogleAnalytics({ 
-  MEASUREMENT_ID,
-}: { 
-  MEASUREMENT_ID: string | undefined,
-}){
+export default function GoogleAnalytics(){
   return (
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}`}
       />
 
       <Script id='google-analytics' strategy="afterInteractive"> {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${MEASUREMENT_ID}', {
+          gtag('config', '${process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID}', {
           page_path: window.location.pathname,
           });
       `}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,24 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import GoogleAnalytics from './components/GoogleAnalytics';
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig: { [id: string]: string | undefined } = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+import firebaseConfig from './firebase/config';
+import firebase_app from './firebase/config';
 
 const open_Sans = Open_Sans({ subsets: ['latin'] });
 
@@ -33,13 +17,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  // Initialize Firebase
-  const firebase_app = initializeApp(firebaseConfig);
-  // const analytics = getAnalytics(firebase_app);
-
   return (
     <html lang="en">
-      { firebaseConfig.measurementId && (<GoogleAnalytics MEASUREMENT_ID={firebaseConfig.measurementId}/>) }
+      { firebase_app && (<GoogleAnalytics/>) }
       
       <body className={open_Sans.className}>{children}</body>
     </html>
