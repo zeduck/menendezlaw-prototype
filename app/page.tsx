ab072b4e518dbbe9b151787d7ff3341229f9730c
Roy Zhang <roysmiley.ca@gmail.com>
2023-10-07 20:08:17 -0400

    hmm

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between h-screen">
-      <div className="">
+      <div className="m-12 md:m-16 lg:m-24 text-7xl">
         <p> Menendez Law </p>
       </div>
 
