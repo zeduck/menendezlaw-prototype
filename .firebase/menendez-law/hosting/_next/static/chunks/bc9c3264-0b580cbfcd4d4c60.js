a7665481b9a984ba8233cedf228281e0e31351aa
Roy Zhang <roysmiley.ca@gmail.com>
2023-10-15 02:03:15 -0400

    y

--- .firebase/menendez-law/hosting/_next/static/chunks/bc9c3264-0b580cbfcd4d4c60.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[582],{8910:function(t,n,a){a.d(n,{HhX:function(){return MdCheck}});var e=a(1172);function MdCheck(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"}}]})(t)}}}]);
