7a127457ffaa6adc03d31ed953d849b9f7894ec1
Roy Zhang <roysmiley.ca@gmail.com>
2024-09-15 22:03:42 -0400

    remove firebase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
-import GoogleAnalytics from './components/GoogleAnalytics';
-import firebase_app from './firebase/config';
 
 const open_Sans = Open_Sans({ weight: '500', subsets: ['latin'] });
 
@@ -18,10 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className='scroll-smooth'>
-      <head>
-        { firebase_app && (<GoogleAnalytics/>) }
-        <link rel="canonical" href="https://www.menendezlaw.ca/" />
-      </head>
       
       <body className={open_Sans.className}>
         {children}
